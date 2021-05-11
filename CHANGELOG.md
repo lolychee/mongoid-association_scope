c00caa55c3c6b3919e0a26782affa943ce2e3b51
lychee xing <lolychee@gmail.com>
2021-05-11 09:38:36 +0800

    init commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2021-05-11
+
+- Initial release
---
+++ lib/mongoid/association_scope.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "association_scope/version"
+
+module Mongoid
+  module AssociationScope
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
